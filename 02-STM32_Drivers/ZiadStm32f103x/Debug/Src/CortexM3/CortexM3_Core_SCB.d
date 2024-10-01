3210bc4b64c5513c46ae62685db593ca1aa811e1
Ziad Mohammed Fathy <114193419+ziadmohamed0@users.noreply.github.com>
2024-10-01 03:24:09 +0300

    Delete 02-STM32_Drivers/ZiadStm32f103x directory

--- 02-STM32_Drivers/ZiadStm32f103x/Debug/Src/CortexM3/CortexM3_Core_SCB.d
+++
@@ -1,8 +0,0 @@
-Src/CortexM3/CortexM3_Core_SCB.o: ../Src/CortexM3/CortexM3_Core_SCB.c \
- ../Inc/CortexM3/CortexM3_Core_SCB.h ../Inc/STM32F103xx/STM32F103xx_VEC.h \
- ../Inc/STM32F103xx/../Common/STD_Types.h \
- ../Inc/STM32F103xx/../Common/BIT_MATH.h
-../Inc/CortexM3/CortexM3_Core_SCB.h:
-../Inc/STM32F103xx/STM32F103xx_VEC.h:
-../Inc/STM32F103xx/../Common/STD_Types.h:
-../Inc/STM32F103xx/../Common/BIT_MATH.h:
